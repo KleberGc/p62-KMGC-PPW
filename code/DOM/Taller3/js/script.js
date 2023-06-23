6c0697bd0338435d245c87f316bf70cd09cf7407
KleberGc <kgonzalezc4@est.ups.edu.ec>
2023-06-23 01:04:10 -0500

    Update GG

--- code/DOM/Taller3/js/script.js
+++ code/DOM/Taller3/js/script.js
@@ -1,12 +1,11 @@
-function seleccionar_opcion (opcion){
-  var opcion1 = document.getElementById("opcion1")
-  var opcion2 = document.getElementById("opcion2")
-
-  if (opcion == 1){
-    opcion1.style.zIndex=2
-    opcion2.style.zIndex=1
-  } else if (opcion == 2){
-    opcion1.style.zIndex=1
-    opcion2.style.zIndex=2
-  }
+function cargar_elemento() {
+  var container = document.getElementById('container')
+  var h1 = document.createElement('h1')
+  var texto = document.createTextNode('Kleber González')
+  h1.appendChild(texto)
+  container.appendChild(h1)
+}
+function eliminar_elemento() {
+  var container = document.getElementById('container')
+  container.removeChild(container.lastElementChild)
 }
--- code/DOM/Taller_final/js/script.js
+++ code/DOM/Taller_final/js/script.js
@@ -1,11 +1,12 @@
-function cargarImagen() {
-    var imagenContainer = document.getElementById("imagenContainer");
-    var imagen = document.createElement("img");
-    imagen.src = "ruta/imagen.jpg"; // Reemplaza "ruta/imagen.jpg" con la ruta de tu imagen
-    imagenContainer.appendChild(imagen);
-}
-
-function eliminarImagen() {
-    var imagenContainer = document.getElementById("imagenContainer");
-    imagenContainer.innerHTML = "";
-}
+function cargar_elemento() {
+    var container = document.getElementById('container');
+    var img = document.createElement('img');
+    img.src = 'images/batux.png';
+    container.appendChild(img);
+  }
+  
+  function eliminar_elemento() {
+    var container = document.getElementById('container');
+    container.removeChild(container.lastElementChild);
+  }
+  
