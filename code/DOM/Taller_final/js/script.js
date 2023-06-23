2f36c6c30ea5a1ad88335668675a4cf4e31025e9
KleberGc <kgonzalezc4@est.ups.edu.ec>
2023-06-23 10:29:59 -0500

    Updates

--- code/DOM/Taller_final/js/script.js
+++ code/DOM/Taller_final/js/script.js
@@ -1,12 +1,18 @@
+var imagenCargada = false; // Variable para verificar si la imagen ya ha sido cargada
+
 function cargar_elemento() {
+  if (!imagenCargada) {
     var container = document.getElementById('container');
     var img = document.createElement('img');
-    img.src = 'images/batux.png';
+    img.src = 'images/tux_ironman.png';
     container.appendChild(img);
+    imagenCargada = true; // Marcar la imagen como cargada
   }
-  
-  function eliminar_elemento() {
-    var container = document.getElementById('container');
-    container.removeChild(container.lastElementChild);
-  }
+}
+
+function eliminar_elemento() {
+  var container = document.getElementById('container');
+  container.removeChild(container.lastElementChild);
+  imagenCargada = false; // Restablecer la variable para permitir cargar una nueva imagen
+}
   
